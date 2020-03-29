dea4e33c143430a6c428884dd1c3bf104e0ca92a
Alix Akhribi <AlixAkhribi@yahoo.com>
2020-03-28 19:47:40 -0700

    Add try catch to converter

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
+const fs = require('fs');
 const imagemin = require('imagemin'); // The imagemin module.
 const imageminWebp = require('imagemin-webp'); // imagemin's WebP plugin.
 const outputFolder = `./output`; // Output folder
 const JPEGImages = `./img/*.jpg`; // JPEG images
 
 const converter = async (pathOfImages, inputImageFormat) => {
-  await imagemin([pathOfImages], {
-    destination: outputFolder,
-    plugins: [
-      imageminWebp({
-        quality: 80 // Quality setting from 0 to 100
-      })
-    ]
-  });
+  try {
+    await imagemin([pathOfImages], {
+      destination: outputFolder,
+      plugins: [
+        imageminWebp({
+          quality: 80 // Quality setting from 0 to 100
+        })
+      ]
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 converter(JPEGImages);
